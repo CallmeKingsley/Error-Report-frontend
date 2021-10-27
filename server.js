5a9b9a877a1f22d0c86e23b06b6f8ba83ed2b93a
kingsley <kingsleyakpan18@gmail.com>
2021-10-27 10:08:19 -0400

    heroku attempt

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const path = require('path')
+
+
+const port = process.env.PORT || 1800
+
+//app.use(express.static(path.join(__dirname, 'client','Home')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'))
+    
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'build', 'index.html')) // relative path
+    })
+}
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log('connected')
+})
